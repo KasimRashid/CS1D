4bcf5122bbab296fac43b597192a87fdc1cc0584
KasimRashid <115325848+KasimRashid@users.noreply.github.com>
2023-01-23 23:56:12 +0000

    here is Kasim's commit

---
+++ hello2Function.cpp
@@ -0,0 +1,12 @@
+/* 
+ * Kasim Rashid 
+ * hello2Function.cpp 
+ * CS 1D
+ */ 
+#include <iostream> 
+using namespace std; 
+int hello2Function(string name2) 
+{ 
+ cout << "hello World " << name2 << endl; 
+ return 0; 
+}
